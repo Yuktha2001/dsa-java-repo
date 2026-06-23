9511a6d2fae905f6e75616964c93942a43843acf
Yuktha2001 <yukthanavyasriabbireddy@gmail.com>
2026-06-23 20:09:20 +0530

    hW

---
+++ hello world.java
@@ -0,0 +1,6 @@
+public class Day01_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, GitHub!");
+        System.out.println("Day 1 of Java Learning Journey");
+    }
+}
